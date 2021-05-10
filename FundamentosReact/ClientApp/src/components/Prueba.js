15e4c56b9df26c1b1115cec22cbf63ce80637c0d
Enrique Palacios <epalaciosc@pixisproti.cl>
2021-05-10 15:22:07 -0400

    Agregar archivos de proyecto.

---
+++ FundamentosReact/ClientApp/src/components/Prueba.js
@@ -0,0 +1,30 @@
+﻿import React, { useState, useEffect } from 'react'
+import { Card, CardBody } from 'reactstrap'
+
+const Prueba = () => {
+    const [nombre, setNombre] = useState('Enrique')
+    const [anio, setAnio] = useState(2021)
+    // const [,] = useState(<valor predeterminado>) => tiene esta forma
+
+    // utilizar una técnica de hooks (estandarizada) para cambiar valores
+    // cuando se termine de cargar la página => Cuando se renderiza la página
+    useEffect(() => {
+        // se gatilla cuando se termina de cargar renderizar la página
+        setNombre('Juan')
+    }, [])
+
+    // useEffect(() => { }, []) => tiene esa forma
+    return (
+        <>
+            <Card>
+                <CardBody>
+                    <h1>Esta es una prueba!</h1>
+                    <h2>Bienvenido {nombre} al {anio} !!</h2>
+                    <p>Esto NO arrojará error</p>
+                </CardBody>
+            </Card>
+        </>
+    )
+}
+
+export default Prueba
